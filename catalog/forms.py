4fc14dd0351d89527799ddd232904502dac20541
blackMastery <black.king1232@gmail.com>
2018-08-14 19:25:27 -0400

    first commit

---
+++ catalog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from django.core.exceptions import ValidationError 
+from django.utils.translation import ugettext_lazy as _
+import datetime 
+
+
+class RenewBookForm(forms.Form):
+	renewal_date = forms.DateField(help_text='Enter a date bewteen now and four weeks')
+
+	def clean_renewal_date(self):
+		data = self.cleaned_date['renewal_date']
+
+		if data < datetime.date.today():
+			raise ValidationError(_('Invalid date - renewal in past'))
+
+		if data > datetime.date.today() + datetime.timedelta(weeks=4):
+			raise ValidationError(_('Invalid date - renewal more than 4 weeks'))\
+
+		return data
+
+
+
+
---
+++ catalog/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+path('', views.index, name='index'),
+path('books/', views.BookListView.as_view(), name='books'),
+path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+
+]
+
+urlpatterns += [   
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+
+urlpatterns += [
+path('librarian/books',views.LibrarianBookView.as_view(), name='librarian-books'),   
+]
+
+urlpatterns += [   
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+
